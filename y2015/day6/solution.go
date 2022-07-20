6be533ec37d259f2659678f9b4d6f9ada5a5fb6e
Gabriel Georgiu <ggeorgiu@uw.co.uk>
2022-07-20 08:58:26 +0300

    Finish day 6

--- y2015/day6/solution.go
+++ y2015/day6/solution.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
 	"aoc/cmd"
 	"aoc/utils"
 )
+
+const gridSize = 1000
 
 type day6 struct{}
 
@@ -22,7 +25,7 @@ func main() {
 
 func doDay() {
 	day := day6{}
-	instr := utils.ReadFileAsStringSlice("/y2015/day6/input_t.txt")
+	instr := utils.ReadFileAsStringSlice("/y2015/day6/input.txt")
 
 	input := cmd.Input{
 		SLI: instr,
@@ -30,90 +33,119 @@ func doDay() {
 
 	fmt.Printf("P1: %d", day.Part1(input))
 	fmt.Println()
-	//fmt.Printf("P2: %d", day.Part2(input))
+	fmt.Printf("P2: %d", day.Part2(input))
 }
 
 func (d day6) Part1(input cmd.Input) interface{} {
-	m := map[point]bool{}
+	m := map[point]int{}
 
 	for _, s := range input.SLI {
 		if strings.HasPrefix(s, "turn on") {
 			p1, p2 := getCoords(s, "turn on ")
-			TurnOn(p1, p2, m)
+			turnOn(m, p1, p2)
 		} else if strings.HasPrefix(s, "turn off") {
 			p1, p2 := getCoords(s, "turn off ")
-			TurnOff(p1, p2, m)
+			turnOff(m, p1, p2)
 		} else if strings.HasPrefix(s, "toggle") {
 			p1, p2 := getCoords(s, "toggle ")
-			Toggle(p1, p2, m)
+			toggle(m, p1, p2)
 		}
-		Print(m)
 	}
 
-	return Count(m)
+	return count(m)
 }
 
 func (d day6) Part2(input cmd.Input) interface{} {
-	return ""
-}
+	m := map[point]int{}
 
-func TurnOn(point1, point2 point, m map[point]bool) {
-	for i := point1.x; i <= point1.y; i++ {
-		for j := point2.x; j <= point2.y; j++ {
-			m[point{i, j}] = true
-		}
-	}
-}
-
-func Count(m map[point]bool) int {
-	c := 0
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			if f, _ := m[point{i, j}]; !f {
-				c++
-			}
+	for _, s := range input.SLI {
+		if strings.HasPrefix(s, "turn on") {
+			p1, p2 := getCoords(s, "turn on ")
+			turnOn2(m, p1, p2)
+		} else if strings.HasPrefix(s, "turn off") {
+			p1, p2 := getCoords(s, "turn off ")
+			turnOff2(m, p1, p2)
+		} else if strings.HasPrefix(s, "toggle") {
+			p1, p2 := getCoords(s, "toggle ")
+			toggle2(m, p1, p2)
 		}
 	}
 
-	return c
+	return count(m)
 }
 
 func getCoords(s string, pref string) (point, point) {
-	strings.ReplaceAll(s, pref, "")
+	s = strings.ReplaceAll(s, pref, "")
 	split := strings.Split(s, " through ")
 	xs := strings.Split(split[0], ",")
 	ys := strings.Split(split[1], ",")
 
 	x1, _ := strconv.Atoi(xs[0])
-	x2, _ := strconv.Atoi(xs[1])
-	y1, _ := strconv.Atoi(ys[0])
+	y1, _ := strconv.Atoi(xs[1])
+	x2, _ := strconv.Atoi(ys[0])
 	y2, _ := strconv.Atoi(ys[1])
 
 	return point{x1, y1}, point{x2, y2}
 }
 
-func TurnOff(point1, point2 point, m map[point]bool) {
-	for i := point1.x; i <= point1.y; i++ {
-		for j := point2.x; j <= point2.y; j++ {
-			m[point{i, j}] = false
+func turnOn(m map[point]int, point1, point2 point) {
+	for i := point1.x; i <= point2.x; i++ {
+		for j := point1.y; j <= point2.y; j++ {
+			m[point{i, j}] = 1
 		}
 	}
 }
 
-func Toggle(point1, point2 point, m map[point]bool) {
-	for i := point1.x; i <= point1.y; i++ {
-		for j := point2.x; j <= point2.y; j++ {
-			m[point{i, j}] = !m[point{i, j}]
+func turnOff(m map[point]int, point1, point2 point) {
+	for i := point1.x; i <= point2.x; i++ {
+		for j := point1.y; j <= point2.y; j++ {
+			m[point{i, j}] = 0
 		}
 	}
 }
 
-func Print(m map[point]bool) {
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			fmt.Printf("%t ", m[point{i, j}])
+func toggle(m map[point]int, point1, point2 point) {
+	for i := point1.x; i <= point2.x; i++ {
+		for j := point1.y; j <= point2.y; j++ {
+			m[point{i, j}] = int(math.Abs(float64(m[point{i, j}] - 1)))
 		}
-		fmt.Println()
 	}
-	fmt.Println()
+}
+
+func turnOn2(m map[point]int, point1, point2 point) {
+	for i := point1.x; i <= point2.x; i++ {
+		for j := point1.y; j <= point2.y; j++ {
+			m[point{i, j}] += 1
+		}
+	}
+}
+
+func turnOff2(m map[point]int, point1, point2 point) {
+	for i := point1.x; i <= point2.x; i++ {
+		for j := point1.y; j <= point2.y; j++ {
+			if v, _ := m[point{i, j}]; v > 0 {
+				m[point{i, j}] -= 1
+			}
+		}
+	}
+}
+
+func toggle2(m map[point]int, point1, point2 point) {
+	for i := point1.x; i <= point2.x; i++ {
+		for j := point1.y; j <= point2.y; j++ {
+			m[point{i, j}] += 2
+		}
+	}
+}
+
+func count(m map[point]int) int {
+	c := 0
+	for i := 0; i < gridSize; i++ {
+		for j := 0; j < gridSize; j++ {
+			v, _ := m[point{i, j}]
+			c += v
+		}
+	}
+
+	return c
 }
--- y2015/day6/solution_test.go
+++ y2015/day6/solution_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"aoc/cmd"
 	"testing"
+
+	"aoc/cmd"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -53,9 +54,31 @@ func TestPart2(t *testing.T) {
 
 func TestUtilities(t *testing.T) {
 	type in struct {
-		x1, x2, y1, y2 int
-		m              map[point]bool
+		x1, y1, x2, y2 int
+		m              map[point]int
 	}
+
+	t.Run("getCoords", func(t *testing.T) {
+		var tests = map[string]struct {
+			input      string
+			expectedP1 point
+			expectedP2 point
+		}{
+			"turn on": {
+				input:      "turn on 1,2 through 98,99",
+				expectedP1: point{1, 2},
+				expectedP2: point{98, 99},
+			},
+		}
+
+		for name, tt := range tests {
+			t.Run(name, func(t *testing.T) {
+				p1, p2 := getCoords(tt.input, "turn on ")
+				assert.Equal(t, tt.expectedP1, p1)
+				assert.Equal(t, tt.expectedP2, p2)
+			})
+		}
+	})
 
 	t.Run("turn on", func(t *testing.T) {
 		var tests = map[string]struct {
@@ -64,41 +87,42 @@ func TestUtilities(t *testing.T) {
 		}{
 			"1": {
 				input: in{
-					0, 0, 0, 5, map[point]bool{},
+					0, 0, 0, 5, map[point]int{},
 				},
-				expected: map[point]bool{
-					point{0, 0}: true,
-					point{0, 1}: true,
-					point{0, 2}: true,
-					point{0, 3}: true,
-					point{0, 4}: true,
-					point{0, 5}: true,
+				expected: map[point]int{
+					point{0, 0}: 1,
+					point{0, 1}: 1,
+					point{0, 2}: 1,
+					point{0, 3}: 1,
+					point{0, 4}: 1,
+					point{0, 5}: 1,
 				},
 			},
 			"2": {
 				input: in{
-					0, 0, 2, 2, map[point]bool{},
+					0, 0, 2, 2, map[point]int{},
 				},
-				expected: map[point]bool{
-					point{0, 0}: true,
-					point{0, 1}: true,
-					point{0, 2}: true,
-					point{1, 0}: true,
-					point{1, 1}: true,
-					point{1, 2}: true,
-					point{2, 0}: true,
-					point{2, 1}: true,
-					point{2, 2}: true,
+				expected: map[point]int{
+					point{0, 0}: 1,
+					point{0, 1}: 1,
+					point{0, 2}: 1,
+					point{1, 0}: 1,
+					point{1, 1}: 1,
+					point{1, 2}: 1,
+					point{2, 0}: 1,
+					point{2, 1}: 1,
+					point{2, 2}: 1,
 				},
 			},
 		}
 		for name, tt := range tests {
 			t.Run(name, func(t *testing.T) {
-				TurnOn(point{tt.input.x1, tt.input.y1}, point{tt.input.x2, tt.input.y2}, tt.input.m)
+				turnOn(tt.input.m, point{tt.input.x1, tt.input.y1}, point{tt.input.x2, tt.input.y2})
 
 				assert.Equal(t, tt.expected, tt.input.m)
 			})
 		}
+
 	})
 
 }
